40d834752ee5987f5b3d1d339992b80d525b99b2
DatJeeves <57726688+DatJeeves@users.noreply.github.com>
2020-05-04 12:21:09 -0500

    Aded methoed to store binary data

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Assignment5
  
+
+ References:
+ - https://www.includehelp.com/code-snippets/cpp-program-to-write-and-read-an-object-in-from-a-binary-file.aspx
+
  Additional Questions to Asks
  - Do we need a default TreeNode constructore with no parameters since we never use it ?
  - Should we have an Update function in addition to insert ? Or should insert replace the 
--- undoList.cpp
+++ undoList.cpp
@@ -23,6 +23,7 @@ void UndoList::tempdo(string cmd) {
     file.open(tempName);
     file << "Need to add in the tables here";
     file.close();
+    
 }
 
 string UndoList::undo() {
@@ -47,7 +48,14 @@ bool UndoList::isEmpty() {
 }
 
 void UndoList::showUndo() {
-
+    // This displays the last command that can be undone
+    UndoNode temp;
+    string line;
+    if (!stack.isEmpty()) {
+        temp = stack.pop();
+        cout << "command : " << temp.command << endl;
+        stack.push(temp);
+    }
 }
 
 int main() {
@@ -62,9 +70,56 @@ int main() {
         undo.tempdo(cmd);
     }
 
+    //write object into the file
+    fstream file;
+    file.open("undolist", ios::out | ios::binary);
+    if (!file) {
+        cout << "Error in creating file...\n";
+        return -1;
+    }
+
+    file.write((char*)&undo, sizeof(undo));
+    file.close();
+    cout << "Date saved into file the file.\n";
+
+
+    cout << "==========" << endl;
+    cout << "Whats the undo ?" << endl;
+    undo.showUndo();
+
+    cout << "==========" << endl;
+    cout << "Whats the undo ?" << endl;
+    undo.showUndo();
+
     cout << "Heres what you got" << endl;
     while (!undo.isEmpty()) {
         cout << undo.undo() << endl;
     }
 
+    //open file again 
+    file.open("undolist", ios::in | ios::binary);
+    if (!file) {
+        cout << "Error in opening file...\n";
+        return -1;
+    }
+
+    if (file.read((char*)&undo, sizeof(undo))) {
+        cout << endl << endl;
+        cout << "Data extracted from file..\n";
+        //print the object
+        undo.showUndo();
+
+        for (int i = 0; i < 5; ++i) {
+            cmd = "This cmd is " + to_string(i);
+
+            cout << "The new cmd is " << cmd << endl;
+            undo.tempdo(cmd);
+        }
+    }
+    else {
+        cout << "Error in reading data from file...\n";
+        return -1;
+    }
+
+    file.close();
 }
