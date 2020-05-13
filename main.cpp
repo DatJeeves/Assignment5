d56019af06eea64e3bc65062fa35f79ed33f5102
DatJeeves <57726688+DatJeeves@users.noreply.github.com>
2020-05-12 20:15:57 -0500

    removed main2 to main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
-#include "treeNode.cpp"
-#include "student.cpp"
-/* #include "bst.cpp" */
+#include "sim.cpp"
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	Student* s = new Student(1234, 2345, 3.8, "Jeevan Acharya", "Big man", "Computer");
-	Student* tempStudent;
-//	BST* testTree = new BST();
-	TreeNode<Student> testNode(s->studentID,s);
-	cout << testNode.key << " is your key" << endl;
-	tempStudent = testNode.data;
 
-	cout << "Student name is "<< tempStudent->name << endl;
-//	delete testTree;
-	//TreeNode test = new 
+
+int main(int argc, char** argv) {
+
+
+	Sim db;
+
+	db.Start();
+	
 	return 0;
+	
 }
